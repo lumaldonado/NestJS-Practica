77a75a308a3435cdade06262862aa0c4fb4eb50e
lumaldonado <lulimaldonado98@hotmail.com>
2023-03-27 17:10:36 -0300

    Se agrega el file editorconfig para crear el formate con el cual se formateara el codigo

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //el 3000 hace referencia al puerto que estamos usando
   await app.listen(3000);
 }
 bootstrap();
